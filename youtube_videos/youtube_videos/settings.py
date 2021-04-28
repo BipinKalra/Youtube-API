85dd2e8f90ae0490b4ba41e32c567543ca2023aa
BipinKalra <bipinkalra@gmail.com>
2021-04-28 18:30:38 +0530

    add: Youtube API key added from dev console

--- youtube_videos/youtube_videos/settings.py
+++ youtube_videos/youtube_videos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youtube_api_cron',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+YOUTUBE_DATA_API_KEY = 'AIzaSyAWHWi_8QQ_411QYvFfC1gMJohnQ7B2z00'
